39aa9e72874215ac2898f788abf7ea4563e3914f
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-06-24 10:15:35 +1200

    Add codeium plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,21 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'Exafunction/codeium.vim',
+  config = function()
+    -- Change '<C-g>' here to any keycode you like.
+    vim.keymap.set('i', '<C-g>', function()
+      return vim.fn['codeium#Accept']()
+    end, { expr = true, silent = true })
+    vim.keymap.set('i', '<c-;>', function()
+      return vim.fn['codeium#CycleCompletions'](1)
+    end, { expr = true, silent = true })
+    vim.keymap.set('i', '<c-,>', function()
+      return vim.fn['codeium#CycleCompletions'](-1)
+    end, { expr = true, silent = true })
+    vim.keymap.set('i', '<c-x>', function()
+      return vim.fn['codeium#Clear']()
+    end, { expr = true, silent = true })
+  end,
+}
